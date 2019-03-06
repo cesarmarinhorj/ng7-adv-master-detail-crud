929e17bbe83acef3e53df2156b49b16a5fd94cb1
Cesar Marinho <cesarmarinhorj@gmail.com>
2019-03-06 20:16:22 -0300

    . adição do modelo de categorias . correção no mock para ficar de acordo com a classe de model

--- src/app/in-memory-database.ts
+++ src/app/in-memory-database.ts
@@ -1,11 +1,12 @@
 import { InMemoryDbService } from "angular-in-memory-web-api";
+import { Categoria } from "./paginas/categorias/shared/models/categoria";
 
 export class InMemoryDatabase implements InMemoryDbService {
     createDb(){
-        const Categorias = [
-            { id: 1, nome: 'Lazer', descricao: 'descrição da categoria lazer' },
-            { id: 2, nome: 'Cozinha', descricao: 'descrição da categoria cozinha' },
-            { id: 3, nome: 'Sala', descricao: 'descrição da categoria sala' },
+        const Categorias: Categoria[] = [
+            { id: '1', nome: 'Lazer', descricao: 'descrição da categoria lazer' },
+            { id: '2', nome: 'Cozinha', descricao: 'descrição da categoria cozinha' },
+            { id: '3', nome: 'Sala', descricao: 'descrição da categoria sala' },
         ];
 
         return { Categorias };
---
+++ src/app/paginas/categorias/shared/models/categoria.ts
@@ -0,0 +1,15 @@
+export class Categoria {
+    id: string;
+    nome: string;
+    descricao: string;
+
+    constructor(
+        id: string,
+        nome: string,
+        descricao: string
+    ){
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+}
