85ac1b9d490c33815eb4b9e400ce664e01842693
Cesar Marinho <cesarmarinhorj@gmail.com>
2019-03-06 16:34:50 -0300

    . listagem das categorias com o breadcrumb . adição de titulo . botão de nova categoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'finansys';
+  title = 'Finan$y$ - O seu sistema contábil favorito!';
 }
